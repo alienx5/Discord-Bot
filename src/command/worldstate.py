05b27996edfebf0fc9b0249719d86a89c48529d7
alienx5 <95942755+alienx5@users.noreply.github.com>
2024-06-12 18:47:38 -0500

    Added a command to display the current state in cetus

--- src/command/worldstate.py
+++ src/command/worldstate.py
@@ -21,12 +21,30 @@ class WorldState(commands.Cog):
                 price = deal_dict['originalPrice']
                 sale = deal_dict['salePrice']
 
-                response =f"Today Darvo is selling {item} for {sale}, original price: {price}."
+                response =f"Today Darvo is selling {item} for {sale}p, original price: {price}p."
             else:
                 response = "Sorry operator, I am having trouble accessing this information."
         else:
             response = "Darvo is not currently available."
 
+        await ctx.send(response)
+
+    @commands.command(name = 'cetus', help = 'Displays the current state in Cetus and the time remaining.')
+    async def cetus_state(self, ctx):
+        url = base_url + '/cetusCycle'
+        cetus = get(url).json()
+
+        if 'error' in cetus:
+            response = "I am having trouble contacting Konzu operator."
+        else:
+            state = cetus['state']
+            time_left = cetus['timeLeft']
+
+            if state == 'night':
+                response = f"It is currently {state} in Cetus for {time_left}. Happy Eidolon hunting operator!"
+            else:
+                reponse = f"It is currently {state} in Cetus for {time_left}."
+        
         await ctx.send(response)
 
 async def setup(bot):
